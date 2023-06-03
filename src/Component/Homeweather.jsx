6a34cefc2a5ea7e3ddd9d4ae9be682329e8f074a
PraveenKumar187 <praveensenthil1998@gmail.com>
2023-06-03 10:47:20 +0530

    added

--- src/Component/Homeweather.jsx
+++ src/Component/Homeweather.jsx
@@ -5,52 +5,52 @@ const Homeweather = () => {
     let place= useRef();
 
     let [weather,setweather]=useState();
+     let [bending,setbending]=useState(false);
     
     let Weatherdetail=()=>{
-
-        const url = `https://ai-weather-by-meteosource.p.rapidapi.com/find_places?text=${place.current.value}&language=en`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '6f69ec1158msh6bc77e47779c7afp1f6c9fjsn705b74fba968',
-            'X-RapidAPI-Host': 'ai-weather-by-meteosource.p.rapidapi.com'
-        }
-    };
+          setbending(true)
+      const url = `https://ai-weather-by-meteosource.p.rapidapi.com/find_places?text=${place.current.value}&language=en`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5aafff0ddfmsh3ddb0947c9792f5p183390jsna3d7176d8edb',
+		'X-RapidAPI-Host': 'ai-weather-by-meteosource.p.rapidapi.com'
+	}
+};
     
                 fetch(url,options)
     .then((res)=>{ return res.json()})
     .then((data)=>{ console.log(data[0]);
-        const url1 = `https://ai-weather-by-meteosource.p.rapidapi.com/current?place_id=${data[0].place_id}&timezone=auto&language=en&units=auto`;
+        const url1= `https://ai-weather-by-meteosource.p.rapidapi.com/current?lat=${data[0].lat}&lon=${data[0].lon}&timezone=auto&language=en&units=auto`;
         const options1 = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': '6f69ec1158msh6bc77e47779c7afp1f6c9fjsn705b74fba968',
+                'X-RapidAPI-Key': '5aafff0ddfmsh3ddb0947c9792f5p183390jsna3d7176d8edb',
                 'X-RapidAPI-Host': 'ai-weather-by-meteosource.p.rapidapi.com'
             }
         };
         
+        
         fetch(url1,options1)
             .then((res)=>{return res.json()})
             .then((data)=>{ console.log(data);
-            setweather(data.current)})        
+            setweather(data.current)
+        setbending(false)})        
     
     })
-
-
-
-
-
-    }
+    
+  }
     
     
     return ( 
-        <div>
+        <div id="bgimg">
             
             <input type="text" placeholder="enter place name " ref={place} />
             <button onClick={Weatherdetail}> submit</button>
-          
+           
+{weather==null && bending   && <h1>loading.........</h1>}
 {
-   weather !=null && 
+   weather !=null &&
     <div>
        
         <div className="main"> 
@@ -63,7 +63,7 @@ const Homeweather = () => {
         <h5> summary :{weather.summary}</h5>
         <h5>humidity :{weather.humidity}</h5>
         </div>
-        </div>
+        </div> 
 
 
     </div>
